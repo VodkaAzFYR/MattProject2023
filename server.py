bf4b5cc76f9796c6740190607c9f9d901fac571f
VodkaAzFYR <antossmaga2007@gmail.com>
2023-05-20 15:12:10 +0200

    Make requests more pretty

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 import socket
-from config import ip, port, receive_message, send_message, generate_numbers, add_to_history
+from config import ip, port, receive_message, send_message, generate_numbers, add_to_history, generate_request
 
 while True:
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
@@ -9,19 +9,28 @@ while True:
         client, addr = s.accept()
         print(f"CONNECTED WITH {addr[0]}")
 
-        send_message(client, 'Connected with server!')
+        hello_mess = generate_request(mode="s", message=f"[SERVER] HELLO CLIENT {addr[0]}")
+        send_message(client, hello_mess)
+
+        receive_hello = receive_message(client)
+        receive_hello = receive_hello.split(r"\r\n")[-1][2:]
+        print(receive_hello)
+
+    #PLAY\r\n-f-n1,2,3,4,5,6
 
         input_user = receive_message(client)
         curr_number = [int(x) for x in generate_numbers().split(',')]
-        guess = [int(x) for x in input_user.split(',')]
-        print(curr_number)
-        print(guess)
+        split_input_user = input_user.split(r"\r\n")
+        guess = split_input_user[-1].split("\r\n\r\n")[0]
+        guess = guess[2:]
+
         good_guesses = 0
         for i in guess:
             if i in curr_number:
                 good_guesses += 1
         if good_guesses > 0:
-            response = f'You won!, you guessed: {good_guesses}'
+            message = f'[SERVER] You won!, you guessed: {good_guesses}'
+            response = generate_request(mode="s", message=message)
             send_message(client, response)
             add_to_history(guess, curr_number)
             with open(r'./history.txt', 'r') as file:
@@ -30,12 +39,14 @@ while True:
             send_message(client, history)
 
         else:
-            response = 'You lost! again losser'
+            message = f'[SERVER] You lost!'
+            response = generate_request(mode="s", message=message)
             send_message(client, response)
+
             add_to_history(guess, curr_number)
             with open(r'./history.txt', 'r') as file:
                 history = (file.read())
             send_message(client, history)
 
     s.close()
-    
+
